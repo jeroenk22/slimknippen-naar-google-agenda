256aee56a44b7b3807db4920f066c332b6b32d33
Jeroen Krajenbrink <jeroenkrajenbrink@gmail.com>
2025-01-28 13:47:13 +0100

    Changed some references from background.js to serviceWorker.js

--- serviceWorker.js
+++ serviceWorker.js
@@ -3,10 +3,10 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
         chrome.identity.getAuthToken({ interactive: true }, token => {
             console.log('getauthtoken is: ', chrome.identity.getAuthToken());
             if (chrome.runtime.lastError || !token) {
-                console.error('Fout bij ophalen token in background.js:', chrome.runtime.lastError);
+                console.error('Fout bij ophalen token in serviceWorker.js:', chrome.runtime.lastError);
                 sendResponse({ success: false, error: chrome.runtime.lastError });
             } else {
-                console.log('Token opgehaald in background.js:', token);
+                console.log('Token opgehaald in serviceWorker.js:', token);
                 sendResponse({ success: true, token });
             }
         });
